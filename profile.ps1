c984b66ff3f2b32932cb6ef1a261ae31b4b7e04a
Ivan Saloponov <ivan.saloponov@acumatica.com>
2018-10-09 07:20:38 +0300

    Add TensorFlow enviroment path

--- profile.ps1
+++ profile.ps1
@@ -1,2 +1,3 @@
 $Env:Path = $Env:Path + "C:\Program Files\CakeBuilder;"
 $Env:Path = $Env:Path + "C:\Program Files\NuGet;"
+$Env:Path = $Env:Path + "C:\Program Files (x86)\Microsoft Visual Studio\Shared\Python36_64\Scripts;C:\Program Files (x86)\Microsoft Visual Studio\Shared\Python36_64\;C:\TF\bazel;C:\TF\msys64;"
