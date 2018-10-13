cf7329b7412034eb6917f275e60cdfafb495e186
Ivan Saloponov <ivan.saloponov@acumatica.com>
2018-10-13 11:29:10 +0300

    Python Path enviroment settings

--- profile.ps1
+++ profile.ps1
@@ -1,3 +1,4 @@
 $Env:Path = $Env:Path + "C:\Program Files\CakeBuilder;"
 $Env:Path = $Env:Path + "C:\Program Files\NuGet;"
 $Env:Path = $Env:Path + "C:\Program Files (x86)\Microsoft Visual Studio\Shared\Python36_64\Scripts;C:\Program Files (x86)\Microsoft Visual Studio\Shared\Python36_64\;C:\TF\bazel;C:\TF\msys64;"
+$Env:Path = $Env:Path + "C:\Program Files\7-Zip;";
